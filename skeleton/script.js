3fcde7b57e5fcced3f8ebc86daa1802af61a8187
mark doerr <mark.doerr@uni-greifswald.de>
2019-10-12 12:44:31 +0200

    basic skeleton of app working

---
+++ skeleton/script.js
@@ -0,0 +1,110 @@
+
+var data = {
+  vocabulary: vocabulary,
+  results: {correct_words: 0},
+  title: 'Vocabulary test',
+  answer: '',
+  curr_item: 0,
+  correct: false,
+  checked: false,
+};
+
+/**
+ * Declaring components
+ */
+
+var QuestionComponent = Vue.extend({
+  data: function () {
+    return data;
+  // var item = vocabulary[Math.floor(Math.random()*vocabulary.length)]
+  // this.curr_item = Math.floor(Math.random() * this.vocabulary.length);
+  // vocabulary[Math.floor(Math.random()*vocabulary.length)].en
+  },
+  template:   '<div class="vocab-question">' +
+  '                 {{ vocabulary[curr_item].en }}' +
+  '            </div>'
+
+});
+
+var VocabAnswerComponent = Vue.extend({
+  data: function () {
+    return data;
+  },
+  methods: {
+    checkVocab: function () {
+      var text;
+
+      text = this.answer.trim();
+      if (!this.checked && text) {
+        this.correct = text === this.vocabulary[this.curr_item].de;
+        if (this.correct) this.results.correct_words++;
+        this.checked = true;
+      }
+    }
+  },
+  // props: ['answer' , 'correct' ],
+  template:
+  '<div class="input-group">'                                                                                        +
+    '<input v-model="answer" @keyup.enter="checkVocab" placeholder="check vocab" type="text" class="form-control">'  +
+    '  <span class="input-group-btn">'                                                                               +
+    '    <button @click="checkVocab" class="btn btn-default" type="button">Check !</button>'                          +
+    '  </span>' +
+    '</div>'
+});
+
+var CorrectionFeedbackComponent = Vue.extend({
+  data: function () {
+    return data;
+  },
+  template:   '<div class="vocab-feedback">' +
+              '  <p> {{ checked ?  (correct ? "Richtig" : "Falsch"): ""  }} </p>'  +
+              '</div>'
+});
+
+var NextVocabComponent = Vue.extend({
+  data: function () {
+    return data;
+  },
+  methods: {
+    nextVocab: function () {
+      if(this.checked){
+        if ( this.curr_item < this.vocabulary.length - 1){
+          this.curr_item++;
+        }
+        else {
+          this.curr_item = 0;
+        }
+        this.checked = false;
+        this.correct = false;
+        this.answer = "";
+      }
+    }
+  },
+  template:   '<div class="next-vocab">' +
+              '  <b-button block variant="success" @click="nextVocab"  >Next Word...</b-button>'  +
+              '</div>'
+});
+
+var ResultsComponent = Vue.extend({
+  data: function () {
+    return data;
+  },
+  template:   '<div class="vocab-results">' +
+  '               <p> Results {{results.correct_words}} correct words of {{vocabulary.length}} words </p>' +
+  '               <p> Ergebnis {{results.correct_words}} richtige Wörter von {{vocabulary.length}} Wörter </p>' +
+  '            </div>'
+});
+
+/**
+ * Registering components
+ */
+Vue.component('question-component', QuestionComponent);
+Vue.component('vocab-answer-component', VocabAnswerComponent);
+Vue.component('correction-feedback-component', CorrectionFeedbackComponent);
+Vue.component('next-vocab-component', NextVocabComponent);
+Vue.component('results-component', ResultsComponent);
+
+new Vue({
+  el: '#app',
+  data: data
+});
---
+++ skeleton/vocab_unit1.js
@@ -0,0 +1,16 @@
+var wordtype = {noun: 'noun',
+                verb: 'verb',
+                auxiliary_verb: 'auxiliary_verb',
+                adjective: 'adjective',
+                 adverb: 'adverv' };
+
+var vocabulary = [
+  { en: "tomorrow", de: "morgen", pl:"",
+    en_correct:false, de_correct: false, trained:false, num_errors: 0 },
+  { en: "today", de: "heute", pl:"",
+    en_correct:false, de_correct: false, trained:false, num_errors: 0 },
+  { en: "bird", de: "Vogel", pl:"birds",
+    en_correct:false, de_correct: false, trained:false, num_errors: 0 },
+  { en: "tree", de: "Baum", pl:"trees",
+      en_correct:false, de_correct: false, trained:false, num_errors: 0 },
+]
